273f6c27eb9ed0f55f8e559a15fba7aa036733bd
zeng149407284 <149407284@qq.com>
2021-04-24 10:52:59 +0800

    test

---
+++ common/config/codeception-local.php
@@ -0,0 +1,16 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/main.php',
+    require __DIR__ . '/main-local.php',
+    require __DIR__ . '/test.php',
+    require __DIR__ . '/test-local.php',
+    [
+        'components' => [
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => 'AJ4dkU1pR7_EvBG2qk9rgZO8F-hAGGeb',
+            ],
+        ],
+    ]
+);
